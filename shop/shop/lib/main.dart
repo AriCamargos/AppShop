52c7c12615dcb37134c49035f111172ff84bd826
Ariana Camargos <arianacamargos16@gmail.com>
2023-01-31 22:22:12 -0300

    feat: multiprovider

--- shop/shop/lib/main.dart
+++ shop/shop/lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/product_details_page.dart';
 import 'package:shop/pages/products_oveview_page.dart';
 import 'package:shop/utils/app_routes.dart';
+
+import 'models/cart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,9 +19,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //Cadastro o Provider na árvore de componentes, onde envolvi meu MaterialApp.
-    return ChangeNotifierProvider(
-      //Retorno o ProductList que é onde está o meu ChangeNotifier
-      create: (_) => ProductList(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ProductList(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
